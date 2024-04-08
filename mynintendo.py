88fa6c961e29be7218971bba60cac445ab8da5bd
Ryely Squires <95175586+Ryely-Squires@users.noreply.github.com>
2024-04-08 13:05:41 -0400

    Update mynintendo.py
    Last change accidentally removed comments and didn't change anything...still working on the error.

--- mynintendo.py
+++ mynintendo.py
@@ -61,35 +61,29 @@ async def send_discord_notification(message, webhook_url):
 # Function to check for new rewards and send notifications
 async def check_for_new_rewards(previous_rewards, current_rewards, rewards_with_points, webhook_url, initial_run=False):
     if current_rewards:
-        new_rewards = []
-        removed_rewards = []
-
-        # Compare current rewards with previous rewards
-        for reward in current_rewards:
-            if reward not in previous_rewards:
-                new_rewards.append(reward)
+        # Check for new rewards
+        new_rewards = [reward for reward in current_rewards if reward not in previous_rewards]
         
-        for reward in previous_rewards:
-            if reward not in current_rewards:
-                removed_rewards.append(reward)
-
+        # Check for removed rewards
+        removed_rewards = [reward for reward in previous_rewards if reward not in current_rewards]
+        
         if new_rewards and not initial_run:
             message = ""
             for reward in new_rewards:
                 message += f"New reward available! {reward} - {rewards_with_points[reward]} Platinum Points\n"
             await send_discord_notification(message, webhook_url)
-            return current_rewards
+            return current_rewards  # Return current rewards without listing them
         elif removed_rewards and not initial_run:
             message = ""
             for reward in removed_rewards:
                 message += f"Reward removed: {reward}\n"
             await send_discord_notification(message, webhook_url)
-            return current_rewards
+            return current_rewards  # Return current rewards without listing them
         elif not initial_run:
-            return previous_rewards
+            return previous_rewards  # Return previous rewards without listing them
     else:
         log_message = f"[{datetime.now()}] No rewards currently available.\n"
-        print(log_message, end='')
+        print(log_message, end='')  # Print to console
         log_to_file(log_message)
         return previous_rewards
 
